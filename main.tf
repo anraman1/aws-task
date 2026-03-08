f896c79ef76c71750faca7fd8bbf5f01bac1333b
anraman1 <36751512+anraman1@users.noreply.github.com>
2026-03-08 14:08:33 -0500

    Add resources for route tables and internet gateway

--- main.tf
+++ main.tf
@@ -37,3 +37,48 @@ resource "aws_instance" "db_instance" {
     Name = each.value["name"]
   }
 }
+
+
+resource "aws_route_table" "route_rt" {
+
+  for_each = {
+    for db in var.dbs : db["name"] => db
+  }
+
+  vpc_id = aws_vpc.icg.id
+
+  tags = {
+    Name = each.value["route_table_name"]
+  }
+}
+
+resource "aws_route_table_association" "route_rt_assoc" {
+
+  for_each = {
+    for db in var.dbs : db["name"] => db
+  }
+
+  subnet_id      = aws_subnet.db_subnet[each.key].id
+  route_table_id = aws_route_table.route_rt[each.key].id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.icg.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_route" "public_route" {
+    # add internet gateway route to public route tables
+    for_each = {
+        for db in var.dbs : db["name"] => db
+         if strcontains(db["route_table_name"], "public")   
+    }
+
+    route_table_id         = aws_route_table.route_rt[each.key].id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id             = aws_internet_gateway.igw.id
+}
+
