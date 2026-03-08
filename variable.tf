296f75f650ec4d75a1f75e48c49d98e6f639573c
anraman1 <36751512+anraman1@users.noreply.github.com>
2026-03-08 11:00:12 -0500

    Add variables for EC2 instance configuration

---
+++ variable.tf
@@ -0,0 +1,34 @@
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "dbs" {
+  description = "Declare the EC2 instances information"
+  type        = list(map(string))
+
+  default = [
+    {
+      name        = "ec2-db1"
+      cidr        = "192.168.1.0/24"
+      subnet_name = "private-sub-db1"
+      route_table_name = "private-rt-db1"
+      az          = "us-east-1a"
+    },
+    {
+      name        = "ec2-app2"
+      cidr        = "192.168.2.0/24"
+      subnet_name = "public-sub-app2"
+      route_table_name = "public-rt-app2"
+      az          = "us-east-1b"
+    },
+   {
+      name        = "ec2-app1"
+      cidr        = "192.168.3.0/24"
+      subnet_name = "public-sub-app1"
+      route_table_name = "public-rt-app1"
+      az          = "us-east-1a"
+    }
+    
+  ]
+}
