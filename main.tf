54761fa39f85deee8c4e44dd2e5f0ffa924ca667
anraman1 <anraman1@gmail.com>
2026-03-08 22:18:57 -0500

    task

--- main.tf
+++ main.tf
@@ -191,7 +191,14 @@ resource "aws_security_group" "lb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]   
+} 
 }
+
 
 resource "aws_lb" "app_lb" {
   name               = "app-lb"
