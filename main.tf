da8a562cc49091d4acae63269ecd550031a03428
anraman1 <anraman1@gmail.com>
2026-03-08 19:57:19 -0500

    task

--- main.tf
+++ main.tf
@@ -121,6 +121,31 @@ resource "aws_autoscaling_attachment" "asg_attachment" {
 }
 
 # create a target group and a load balancer to distribute traffic to the app instances in the ASG
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Security group for the load balancer"
+  vpc_id      = aws_vpc.icg.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_lb" "app_lb" {
   name               = "app-lb"
   internal           = false
